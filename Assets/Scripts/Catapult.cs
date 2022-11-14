b6ddbe910bdf4a0efc0e081b227c5128daa276da
arielayb <cyberspace009@gmail.com>
2022-11-13 21:38:14 -0700

    added tracking camera

---
+++ Assets/Scripts/Catapult.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+    private Vector2 launchPos;
+    private Vector2 initialPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) { 
+            
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
